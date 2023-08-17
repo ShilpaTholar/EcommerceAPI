00ac1679c25b58d634ed24dba0fe5c5faa50afe7
ShilpaTholar <2019shilpa.tholar@ves.ac.in>
2023-08-17 23:22:03 +0530

    user register and login

---
+++ routes/authRoute.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import {registerController,loginController} from '../controllers/authController.js';
+import {isAdmin,requireSignIn} from '../middlewares/authMiddleware.js';
+const router=express.Router();
+
+
+
+router.post("/register",registerController);
+
+router.post("/login",loginController);
+
+
+
+
+
+//protected route
+
+router.get('/user-auth',requireSignIn,(req,res)=>{
+
+	res.status(200).send({ok:true});
+});
+router.get('/admin-auth',requireSignIn,isAdmin,(req,res)=>{
+
+	res.status(200).send({ok:true});
+});
+
+
+
+export default router;
